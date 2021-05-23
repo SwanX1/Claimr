4b6ff539414c303c405dc4321a67fb3151a9850a
SwanX1 <karliscern@gmail.com>
2021-05-23 13:13:35 +0300

    Added javadocs for ClaimGroup, better toString method

--- src/main/java/dev/cernavskis/claimr/util/ClaimGroup.java
+++ src/main/java/dev/cernavskis/claimr/util/ClaimGroup.java
@@ -25,15 +25,32 @@ public class ClaimGroup implements IClaimGroup {
     owner = ownerUUID;
   }
 
+  /**
+   * Gets group from created groups
+   * @param name Name of the group to look for
+   * @return IClaimGroup if group exists, null if group doesn't exist.
+   */
   @Nullable
   public static IClaimGroup getGroup(String name) {
     return ClaimData.groups.get(name);
   }
 
+  /**
+   * Gets group from created groups, creates group if it doesn't exist
+   * @param name Name of the group to look for or to create
+   * @param owner Owner of the group, used only for creating a group, doesn't overwrite actual owner
+   * @return IClaimGroup
+   */
   public static IClaimGroup getOrCreateGroup(String name, PlayerEntity owner) {
     return getOrCreateGroup(name, ClaimrUtil.getUUID(owner));
   }
 
+  /**
+   * Gets group from created groups, creates group if it doesn't exist
+   * @param name Name of the group to look for or to create
+   * @param owner Owner of the group, used only for creating a group, doesn't overwrite actual owner
+   * @return IClaimGroup
+   */
   public static IClaimGroup getOrCreateGroup(String name, UUID owner) {
     if (!ClaimData.groups.containsKey(name)) {
       Claimr.DATA.shouldSave = true;
@@ -42,63 +59,115 @@ public class ClaimGroup implements IClaimGroup {
     return ClaimData.groups.get(name);
   }
 
+  /**
+   * Returns name of the group
+   */
   public String getName() {
     return name;
   }
 
+  /** 
+   * Returns the UUID of the owner of the group
+   */
   public UUID getOwner() {
     return owner;
   }
 
+  /**
+   * Sets new owner of the group
+   * @return The UUID of the new owner
+   */
   public UUID setOwner(PlayerEntity player) {
     return setOwner(ClaimrUtil.getUUID(player));
   }
 
+  /**
+   * Sets new owner of the group
+   * @return The UUID of the new owner
+   */
   public UUID setOwner(UUID uuid) {
     this.owner = uuid;
     return this.owner;
   }
 
+  /**
+   * Use this instead of {@code IClaimGroup.getMembers().size()}.
+   * {@link getMembers()} converts the member map to an ImmutableMap, this is faster.
+   */
   public int getMembersSize() {
     return members.size();
   }
 
+  /**
+   * @return True, if the rank of the player is >= 1, or if the player is the owner of the group.
+   */
   public boolean canInteract(PlayerEntity player) {
     return getRank(player) >= 1 || isOwner(player);
   }
 
+  /**
+   * @return True, if the rank of the player is >= 1, or if the player is the owner of the group.
+   */
   public boolean canInteract(UUID uuid) {
     return getRank(uuid) >= 1 || isOwner(uuid);
   }
 
+  /**
+   * @return True, if the rank of the player is >= 2, or if the player is the owner of the group.
+   */
   public boolean canManage(PlayerEntity player) {
     return getRank(player) >= 2 || isOwner(player);
   }
 
+  /**
+   * @return True, if the rank of the player is >= 2, or if the player is the owner of the group.
+   */
   public boolean canManage(UUID uuid) {
     return getRank(uuid) >= 2 || isOwner(uuid);
   }
 
+  /**
+   * @return True, if the player is the owner of the group.
+   */
   public boolean isOwner(PlayerEntity player) {
     return isOwner(ClaimrUtil.getUUID(player));
   }
 
+  /**
+   * @return True, if the player is the owner of the group.
+   */
   public boolean isOwner(UUID uuid) {
     return uuid.equals(getOwner());
   }
 
+  /**
+   * @return The rank of the player, see wiki entry for what each rank means.
+   */
   public int getRank(PlayerEntity player) {
     return getRank(ClaimrUtil.getUUID(player));
   }
 
+  /**
+   * @return The rank of the player, see wiki entry for what each rank means.
+   */
   public int getRank(UUID uuid) {
     return isOwner(uuid) ? 3 : members.getOrDefault(uuid, 0);
   }
 
+  /**
+   * Sets a new rank for the player. If the given player is the owner of the group, rank stays unchanged.
+   * @return The new rank of the player. If rank is less than 0 or greater than 2,
+   * the rank of the player stays unchanged, and the rank of the player is returned.
+   */
   public int setRank(PlayerEntity player, int rank) {
     return setRank(ClaimrUtil.getUUID(player), rank);
   }
 
+  /**
+   * Sets a new rank for the player. If the given player is the owner of the group, rank stays unchanged.
+   * @return The new rank of the player. If rank is less than 0 or greater than 2,
+   * the rank of the player stays unchanged, and the rank of the player is returned.
+   */
   public int setRank(UUID uuid, int rank) {
     if (isOwner(uuid)) {
       return 3;
@@ -130,7 +199,7 @@ public class ClaimGroup implements IClaimGroup {
 
   @Override
   public String toString() {
-    return String.format("{ClaimGroup \"%s\"; %s Members}", getName(), getMembersSize());
+    return String.format("{%s \"%s\"; %s Members}", this.getClass().getName(), getName(), getMembersSize());
   }
 
   public ImmutableMap<UUID, Integer> getMembers() {
@@ -226,6 +295,11 @@ public class ClaimGroup implements IClaimGroup {
     @Override
     public ImmutableMap<UUID, Integer> getMembers() {
       return ImmutableMap.of();
+    }
+
+    @Override
+    public String toString() {
+      return String.format("{%s \"%s\"}", this.getClass().getName(), getName());
     }
   }
 }
